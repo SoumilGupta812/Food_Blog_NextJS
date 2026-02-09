c70fb3c1a9904ecf47577c87062c3762c948de40
blac-coffee <soumilgupta.812@gmail.com>
2026-02-09 21:26:07 +0530

    first commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,32 @@
+import { GetMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+export default async function MealDetailsPage({ params }) {
+  const { slug } = await params;
+  const meal = GetMeal(slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailtp:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "@/components/main-header/main-header.module.css";
+import Image from "next/image";
+import NavLink from "./nav-link";
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logoImg} alt="A plate with food on it." />
+        NextLevel Foodies
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink href="/meals">Browse Meals</NavLink>
+          </li>
+          <li>
+            <NavLink href="/community">Foodies Community</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
